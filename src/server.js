2809be0bb1c6834405a242a393587964afe379ca
abdal20 <abdal.yezidi@gmail.com>
2025-09-06 11:35:36 -0400

    third commit

--- src/server.js
+++ src/server.js
@@ -57,7 +57,7 @@ app.use('*', (req, res) => {
   res.status(404).json({ success: false, message: 'Route not found' });
 });
 
-const PORT = env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
